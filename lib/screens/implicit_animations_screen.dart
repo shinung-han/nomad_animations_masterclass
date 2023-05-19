ce6e08644b7cc47563b62a1435e8a364252d2001
Shinung Han <hsuj86@gmail.com>
2023-05-19 10:58:26 +0900

    1.1 Implicitly Animated Widgets + 1.2 AnimatedContainer

--- lib/screens/implicit_animations_screen.dart
+++ lib/screens/implicit_animations_screen.dart
@@ -9,8 +9,45 @@ class ImplicitAnimationsSCreen extends StatefulWidget {
 }
 
 class _ImplicitAnimationsSCreenState extends State<ImplicitAnimationsSCreen> {
+  bool _visible = true;
+
+  void _goTrigger() {
+    setState(() {
+      _visible = !_visible;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Implicit Animations'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedContainer(
+              duration: const Duration(seconds: 2),
+              width: size.width * 0.8,
+              height: size.width * 0.8,
+              transform: Matrix4.rotationZ(_visible ? 1 : 0),
+              transformAlignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: _visible ? Colors.red : Colors.amber,
+                borderRadius: BorderRadius.circular(_visible ? 100 : 0),
+              ),
+            ),
+            const SizedBox(height: 50),
+            ElevatedButton(
+              onPressed: _goTrigger,
+              child: const Text('Go!'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
